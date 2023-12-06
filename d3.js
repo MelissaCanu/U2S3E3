fd073d5d5524c8a4869a6a402f0570849abc6da4
MelissaCanu <melissamcanu@gmail.com>
2023-12-06 15:02:16 +0100

    creando (spero) dinamicamente le card DOM manip

--- d3.js
+++ d3.js
@@ -8,4 +8,27 @@ fetch("https://striveschool-api.herokuapp.com/books")
   /* adesso sono sicura che ho i dati (controllato su network in inspector) */
   .then((booksObj) => {
     /* qua manipolo i dati ad esempio con la DOM manipulation */
+    const booksContainer = document.getElementById("booksContainer");
+    const booksRow = document.createElement("div");
+    booksRow.className = "row";
+    const booksCol = document.createElement("div");
+    booksCol.className = "col-12 col-md-6 col-lg-4";
+
+    books.forEach((book) => {
+      const card = document.createElement("div");
+      card.className = "card";
+    });
   });
+
+/*           <div class="container text-center m-4">
+      <div class="row align-items-start">
+        <div class="col">
+          <div class="card" style="width: 18rem">
+            <img src="..." class="card-img-top" alt="..." />
+            <div class="card-body">
+              <h5 class="card-title"></h5>
+              <p class="card-price"></p>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+          </div>
+        </div> */
