02410fe9c0a98c7784e1ec76a0326838c8b7a856
MelissaCanu <melissamcanu@gmail.com>
2023-12-06 14:34:21 +0100

    second .then within fetch

--- d3.js
+++ d3.js
@@ -1,1 +1,11 @@
-fetch("https://striveschool-api.herokuapp.com/books").then((booksObj) => {});
+fetch("https://striveschool-api.herokuapp.com/books")
+  .then((booksObj) => {
+    /* tramite parametro della callback ricevo il valore del fetch qua */
+    if (booksObj.ok) {
+      return booksObj.json(); /* parso la risposta */
+    }
+  })
+  /* adesso sono sicura che ho i dati (controllato su network in inspector) */
+  .then((booksObj) => {
+    /* qua manipolo i dati ad esempio con la DOM manipulation */
+  });
