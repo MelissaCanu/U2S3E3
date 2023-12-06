8c2442a8ff5edb58bf5c9385f41e2321c162ad33
MelissaCanu <melissamcanu@gmail.com>
2023-12-06 15:22:51 +0100

    dynamic cards created

--- d3.js
+++ d3.js
@@ -6,29 +6,51 @@ fetch("https://striveschool-api.herokuapp.com/books")
     }
   })
   /* adesso sono sicura che ho i dati (controllato su network in inspector) */
-  .then((booksObj) => {
+  .then((books) => {
     /* qua manipolo i dati ad esempio con la DOM manipulation */
     const booksContainer = document.getElementById("booksContainer");
     const booksRow = document.createElement("div");
-    booksRow.className = "row";
+    booksRow.className = "row align-items-start";
     const booksCol = document.createElement("div");
     booksCol.className = "col-12 col-md-6 col-lg-4";
 
+    /* appendo i child ai parent */
+    booksRow.appendChild(booksCol);
+    booksContainer.appendChild(booksRow);
+
     books.forEach((book) => {
+      /* card */
       const card = document.createElement("div");
       card.className = "card";
-    });
-  });
+      /* img */
+      const image = document.createElement("img");
+      image.src = book.img;
+      image.className = "card-img-top";
+      image.alt = book.title;
+      /* card body */
+      const cardBody = document.createElement("div");
+      cardBody.className = "card-body";
+      /* card title */
+      const title = document.createElement("h5");
+      title.className = "card-title";
+      title.textContent = book.title;
+      /* price */
+      const prezzo = document.createElement("p");
+      prezzo.className = "card-text";
+      prezzo.textContent = "Price: " + book.price;
 
-/*           <div class="container text-center m-4">
-      <div class="row align-items-start">
-        <div class="col">
-          <div class="card" style="width: 18rem">
-            <img src="..." class="card-img-top" alt="..." />
-            <div class="card-body">
-              <h5 class="card-title"></h5>
-              <p class="card-price"></p>
-              <a href="#" class="btn btn-primary">Go somewhere</a>
-            </div>
-          </div>
-        </div> */
+      const button = document.createElement("a");
+      button.href = "#";
+      button.className = "btn btn-success";
+      button.textContent = "Buy";
+
+      /* appendo gli elementi */
+      cardBody.appendChild(title);
+      cardBody.appendChild(prezzo);
+      card.appendChild(image);
+      card.appendChild(cardBody);
+      booksCol.appendChild(card);
+    });
+  })
+
+  .catch((error) => console.error("Error fetching books", error));
